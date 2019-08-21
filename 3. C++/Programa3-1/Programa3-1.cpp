be76282903687d2119b4bb424e48804bfa7228dd
Luis Enrique Tapia <tapia641@gmail.com>
2019-08-21 07:31:07 -0500

    Add Programa3-1

---
+++ 3. C++/Programa3-1/Programa3-1.cpp
@@ -0,0 +1,20 @@
+#include "Fecha.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    Fecha a, b, c(12, 9, 1999);
+
+    // COMO NO RECIBE DATOS MUESTRA LOS DATOS DEFAULT EN LA CLASE
+    a.muestraFecha();
+
+    // SE LE INICIALIZAR UNOS DATOS ACTUALIZANDO LOS DEFAULT
+    b.inicializaFecha(17, 6, 2000);
+    b.muestraFecha();
+
+    // MUESTRA LOS DATOS PASADOS CUANDO SE INSTANCIA
+    c.muestraFecha();
+    return 0;
+}
