77bf5f2949bfc7cada38cc9cf9cfd8232572d970
Luis Tapia <tapia@Luiss-Mac.local>
2019-09-03 16:34:08 -0500

    Desempenio Java

--- 7. C++/Desempeño_Java/CalculosNumericos.java
+++ 7. C++/Desempeño_Java/CalculosNumericos.java
@@ -1,15 +1,20 @@
 import java.io.*;
 import java.math.*;
 //import java.lang.ClassNotFoundException;
+import java.util.Scanner;
 
-public class Desempeño
+public class CalculosNumericos
 {
 	public static void main(String[] args){
+
+		Scanner t = new Scanner(System.in);
+		System.out.println("Ingresa N:");
+		double MAX = t.nextInt();
 		double i=0;
 		double seno, coseno, tangente, logaritmo, raizCuad;
 		seno=coseno=tangente=logaritmo=raizCuad=0;
 		//double MAX=300000;//1
-		double MAX=30000000;//2
+		//double MAX=30000000;//2
 		//double MAX=300000000;//3
 
 		while(i<MAX)
@@ -17,7 +22,7 @@ public class Desempeño
 			seno+=Math.sin(i);
 			coseno+=Math.cos(i);
 			tangente+=Math.tan(i);
-			logaritmo+=Math.log10(i);
+			logaritmo+=Math.log10(i+1);
 			raizCuad+=Math.sqrt(i);
 			i++;
 		}
