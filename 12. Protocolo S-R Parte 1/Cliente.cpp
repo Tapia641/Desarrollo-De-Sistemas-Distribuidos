0ed9d15704d691c2add24b7005baae09d883bdf2
tapia641 <tapia641@gmail.com>
2019-10-11 07:14:22 -0500

    Parte 1 finalizada

--- 12. Protocolo S-R Parte 1/Cliente.cpp
+++ 12. Protocolo S-R Parte 1/Cliente.cpp
@@ -8,16 +8,21 @@ int main(int argc, char*argv[]) {
 
 	if (argc < 3)
 	{
-		perror("USO: ./Cliente IP PUERTO");
+		perror("USO: ./Cliente IP PUERTO N");
 	}else{
 		char arreglo[100] = "Hola soy el cliente \0";
 		char *ip = argv[1];
 		int puerto = atoi(argv[2]);
 		int operacion = 0;
+		int N = atoi(argv[3]);
 
 		Solicitud cliente;
 
-		printf("La respuesta del servidor es :\n%s\n", cliente.doOperation(ip, puerto, operacion, arreglo));
+		while (N--)
+		{
+			printf("La  respuesta del servidor es :\n%s\n", cliente.doOperation(ip, puerto, operacion, arreglo));
+		}
+		
 	}
 
 	return 0;
